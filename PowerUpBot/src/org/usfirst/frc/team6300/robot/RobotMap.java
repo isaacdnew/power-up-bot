1f60207a0be286919e5abed4c400cb9f9ecff5fa
isaac <isaac@eyelow>
2018-02-02 11:44:50 -0500

    updated drivetrain motor ports to match actual robot

--- PowerUpBot/src/org/usfirst/frc/team6300/robot/RobotMap.java
+++ PowerUpBot/src/org/usfirst/frc/team6300/robot/RobotMap.java
@@ -15,9 +15,9 @@ package org.usfirst.frc.team6300.robot;
  */
 public class RobotMap {
 	//Drivetrain
-	public static final int lfMotor = 0;
-	public static final int rfMotor = 1;
-	public static final int lbMotor = 2;
+	public static final int lfMotor = 1;
+	public static final int rfMotor = 2;
+	public static final int lbMotor = 0;
 	public static final int rbMotor = 3;
 	public static final int lSolPort1 = 0;
 	public static final int lSolPort2 = 1;
