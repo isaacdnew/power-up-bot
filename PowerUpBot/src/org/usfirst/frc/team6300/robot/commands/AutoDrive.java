d376927ec07ab4bc51b6174816996f6d3d11d797
isaac <isaac@eyelow>
2018-02-16 08:51:50 -0500

    made AutoDrive enable the heading control when it runs

--- PowerUpBot/src/org/usfirst/frc/team6300/robot/commands/AutoDrive.java
+++ PowerUpBot/src/org/usfirst/frc/team6300/robot/commands/AutoDrive.java
@@ -11,21 +11,18 @@ import edu.wpi.first.wpilibj.command.Command;
 public class AutoDrive extends Command {
 	private Drivetrain drivetrain;
 	private double seconds;
-	private double power;
-	private boolean lowGear;
+	private double forwardSpeed;
 	
 	/**
 	 * Drives the robot straight forward over a specified distance.
 	 * @param drivetrain the drivetrain to use
-	 * @param power the power (between 0 and 1) to send to the motors
-	 * @param lowGear if true, robot will use low gear. Otherwise, it will use high gear.
-	 * @param seconds the time to drive forward.
+	 * @param forwardSpeed the speed at which to go forward
+	 * @param seconds the time for which to drive.
 	 */
-    public AutoDrive(Drivetrain drivetrain, double power, double seconds) {
+    public AutoDrive(Drivetrain drivetrain, double forwardSpeed, double seconds) {
     	this.drivetrain = drivetrain;
     	this.seconds = seconds;
-    	this.power = power;
-    	this.lowGear = lowGear;
+    	this.forwardSpeed = forwardSpeed;
         requires(drivetrain);
     }
 
@@ -44,13 +41,16 @@ public class AutoDrive extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	drivetrain.setSpeeds(power, power);
+    	drivetrain.shiftDown();
+    	drivetrain.enable();
+    	drivetrain.setSpeeds(forwardSpeed, forwardSpeed);
     	Timer.delay(seconds);
-    	drivetrain.stop();
+    	drivetrain.setSpeeds(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	drivetrain.setSpeeds(0, 0);
     }
 }
