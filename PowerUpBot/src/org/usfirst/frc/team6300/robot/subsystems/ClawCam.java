8195cdbd99651d0f08b056e8739db7f6f6538ca8
isaac <isaac@eyelow>
2018-02-19 17:48:36 -0500

    added cameras

---
+++ PowerUpBot/src/org/usfirst/frc/team6300/robot/subsystems/ClawCam.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc.team6300.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.vision.VisionThread;
+
+/**
+ * The camera that looks at the shooter from the intake bar (for human use only)
+ */
+public class ClawCam extends Subsystem {
+	UsbCamera clawCam;
+	final int imgWidth = 160;
+	final int imgHeight = 120;
+	final int fps = 20;
+	final int brightness = 20;
+	final int exposure = 50;
+	final int whiteBalance = 1000;
+
+	VisionThread visionThread;
+	final Object turnAngleSync = new Object();
+	double centerX = 0.0;
+	double lastTurnAngle = 0.0;
+	double turnAngle = 0.0;
+
+	public ClawCam(int port) {
+		clawCam = new UsbCamera("ShooterCam", port);
+		clawCam.setResolution(imgWidth, imgHeight);
+		clawCam.setFPS(fps);
+		clawCam.setBrightness(brightness);
+		clawCam.setExposureManual(exposure);
+		clawCam.setWhiteBalanceManual(whiteBalance);
+	}
+
+	public void startRecording() {
+		CameraServer.getInstance().startAutomaticCapture(clawCam);
+	}
+
+	public void startProcessing() {
+		startRecording();
+		CvSource outputStream = CameraServer.getInstance().putVideo("ShooterCam", imgWidth, imgHeight);
+		visionThread = new VisionThread(clawCam, new ClawCamPipeline(), pipeline -> {
+			outputStream.putFrame(pipeline.cvFlipOutput());
+		});
+		visionThread.start();
+	}
+
+	public void initDefaultCommand() {
+	}
+}
