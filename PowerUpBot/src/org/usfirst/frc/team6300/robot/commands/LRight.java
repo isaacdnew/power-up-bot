df463e47da646fe5394bc98d19f11208e91b2e0e
isaac <isaac@eyelow>
2018-02-18 21:26:21 -0500

    added LRight

---
+++ PowerUpBot/src/org/usfirst/frc/team6300/robot/commands/LRight.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team6300.robot.commands;
+
+import org.usfirst.frc.team6300.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class LRight extends CommandGroup {
+	public LRight(Robot robot) {
+		// Hold onto the cube
+		addParallel(new CloseClaw(robot.claw));
+
+		// Calibrate arms
+		addSequential(new LiftTo(robot, robot.lifter.startAngle));
+		addSequential(new CalibrateWrist(robot.wrist));
+
+		// Deliver Cube
+		addParallel(new LiftTo(robot, robot.lifter.switchAngle));
+
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.0));
+		addSequential(new Rotate(robot.drivetrain, 90));
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.0));
+
+		addSequential(new OpenClaw(robot.claw));
+	}
+}
