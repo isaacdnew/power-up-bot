b10f16e67ad176dd9e627feb7c451b1854543733
isaac <isaac@eyelow>
2018-02-19 12:34:39 -0500

    Added Winch and TeleWinch

---
+++ PowerUpBot/src/org/usfirst/frc/team6300/robot/commands/TeleWinch.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc.team6300.robot.commands;
+
+import org.usfirst.frc.team6300.robot.OI;
+import org.usfirst.frc.team6300.robot.subsystems.Winch;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class TeleWinch extends Command {
+
+	private Winch winch;
+
+	public TeleWinch(Winch winch) {
+		this.winch = winch;
+		requires(winch);
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+		if (Timer.getMatchTime() < 45) {
+			winch.setMotor(OI.deadZone(OI.cubeJoy.getRawAxis(OI.lTrigAxis)));
+		} else {
+			winch.setMotor(0);
+		}
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
---
+++ PowerUpBot/src/org/usfirst/frc/team6300/robot/subsystems/Winch.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team6300.robot.subsystems;
+
+import org.usfirst.frc.team6300.robot.RobotMap;
+import org.usfirst.frc.team6300.robot.commands.TeleWinch;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Winch extends Subsystem {
+	private SpeedController motor = new VictorSP(RobotMap.lLiftMotor);
+
+	public Winch() {
+		motor.setInverted(RobotMap.liftInverted);
+	}
+
+	public void initDefaultCommand() {
+		setDefaultCommand(new TeleWinch(this));
+	}
+
+	public void setMotor(double power) {
+		motor.set(power);
+	}
+}
