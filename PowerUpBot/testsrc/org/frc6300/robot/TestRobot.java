e2be2f8e58682debd23a43612ff0debe04d03725
isaac <isaac@eyelow>
2018-02-23 12:04:55 -0500

    used strongback to create new Robot.java and testsrc

---
+++ PowerUpBot/testsrc/org/frc6300/robot/TestRobot.java
@@ -0,0 +1,13 @@
+/* Created Fri Feb 23 12:02:56 EST 2018 */
+package org.frc6300.robot;
+
+import org.junit.Test;
+
+public class TestRobot {
+
+    @Test
+    public void test() {
+        // do something here
+    }
+
+}
