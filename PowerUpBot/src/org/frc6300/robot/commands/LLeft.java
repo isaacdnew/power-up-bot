59462d3ecad7d58a70f24ba5b4decc31f4615154
isaac <isaac@eyelow>
2018-03-01 22:57:56 -0500

    uncommented auto commands

--- PowerUpBot/src/org/frc6300/robot/commands/LLeft.java
+++ PowerUpBot/src/org/frc6300/robot/commands/LLeft.java
@@ -1,6 +1,7 @@
 package org.frc6300.robot.commands;
 
 import org.frc6300.robot.Robot;
+import org.frc6300.robot.subsystems.Lifter;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -8,21 +9,21 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class LLeft extends CommandGroup {
-	public LLeft(/*Robot robot*/) {
-//		// Hold onto the cube
-//		addParallel(new CloseClaw(robot.claw));
-//
-//		// Set up arms
-//		addSequential(new LiftTo(robot, robot.lifter.startLength));
-//		addSequential(new CalibrateWrist(robot.wrist));
-//		addParallel(new LiftTo(robot, robot.lifter.switchLength));
-//
-//		// Drive to switch
-//		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.0));
-//		addSequential(new Rotate(robot.drivetrain, 90));
-//		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.0));
-//
-//		// Drop cube on switch
-//		addSequential(new OpenClaw(robot.claw));
+	public LLeft(Robot robot) {
+		// Hold onto the cube
+		addParallel(new CloseClaw(robot.claw));
+
+		// Set up arms
+		addSequential(new LiftTo(robot, Lifter.minLength));
+		addSequential(new CalibrateWrist(robot.wrist));
+		addParallel(new LiftTo(robot, Lifter.switchLength));
+
+		// Drive to switch
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.0));
+		addSequential(new Rotate(robot.drivetrain, 90));
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.0));
+
+		// Drop cube on switch
+		addSequential(new OpenClaw(robot.claw));
 	}
 }
--- PowerUpBot/src/org/frc6300/robot/commands/RLeft.java
+++ PowerUpBot/src/org/frc6300/robot/commands/RLeft.java
@@ -1,6 +1,7 @@
 package org.frc6300.robot.commands;
 
 import org.frc6300.robot.Robot;
+import org.frc6300.robot.subsystems.Lifter;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -8,23 +9,23 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  *
  */
 public class RLeft extends CommandGroup {
-	public RLeft(/*Robot robot*/) {
-//		// Hold onto the cube
-//		addParallel(new CloseClaw(robot.claw));
-//
-//		// Set up arms
-//		addSequential(new LiftTo(robot, robot.lifter.startLength));
-//		addSequential(new CalibrateWrist(robot.wrist));
-//		addParallel(new LiftTo(robot, robot.lifter.switchLength));
-//
-//		// Drive to switch
-//		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.5));
-//		addSequential(new Rotate(robot.drivetrain, 90));
-//		addSequential(new AutoDrive(robot.drivetrain, 0.5, 5.0));
-//		addSequential(new Rotate(robot.drivetrain, 90));
-//		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.5));
-//
-//		// Drop cube on switch
-//		addSequential(new OpenClaw(robot.claw));
+	public RLeft(Robot robot) {
+		// Hold onto the cube
+		addParallel(new CloseClaw(robot.claw));
+
+		// Set up arms
+		addSequential(new LiftTo(robot, Lifter.minLength));
+		addSequential(new CalibrateWrist(robot.wrist));
+		addParallel(new LiftTo(robot, Lifter.switchLength));
+
+		// Drive to switch
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.5));
+		addSequential(new Rotate(robot.drivetrain, 90));
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 5.0));
+		addSequential(new Rotate(robot.drivetrain, 90));
+		addSequential(new AutoDrive(robot.drivetrain, 0.5, 1.5));
+
+		// Drop cube on switch
+		addSequential(new OpenClaw(robot.claw));
 	}
 }
