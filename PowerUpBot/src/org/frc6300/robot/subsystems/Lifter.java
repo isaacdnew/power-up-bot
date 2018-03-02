a40c07d4c4e62efa3dbfd6f1bf7c1253002d94db
isaac <isaac@eyelow>
2018-03-01 22:21:19 -0500

    finished endstops on lifter

--- PowerUpBot/src/org/frc6300/robot/subsystems/Lifter.java
+++ PowerUpBot/src/org/frc6300/robot/subsystems/Lifter.java
@@ -13,13 +13,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Lifter extends PIDSubsystem {
-	// TODO calibrate these angles
+	// TODO calibrate these lengths
 	public static final double minLength = 1;
 	public static final double minVertLength = 1.68;
 	public static final double switchLength = 5.1;
 	public static final double topIllegalLength = 7.8;
 	public static final double scaleMaxLength = 8.7;
 	public static final double maxLength = 9.0;
+	public static final double actuatorMountToElbow = 10;
+	public static final double elbowToRodEnd = 10;
+	public static final double collapsedLength = 10;
 
 	private SpeedController motor = new VictorSP(RobotMap.liftMotor);
 
@@ -28,8 +31,8 @@ public class Lifter extends PIDSubsystem {
 	private Potentiometer pot = new AnalogPotentiometer(RobotMap.liftPot, actuatorRange, potOffset);
 
 	private static final double p = 0.05;
-	private static final double i = 0;
-	private static final double d = 0;
+	private static final double i = 0.0;
+	private static final double d = 0.0;
 	private static final double feedForward = 0;
 	private static final double pidPeriod = 0.005;
 
@@ -53,22 +56,42 @@ public class Lifter extends PIDSubsystem {
 	protected void usePIDOutput(double output) {
 		if (minLength < getPosition() && getPosition() < maxLength) {
 			motor.set(output);
-		} else {
-			motor.set(0);
+			SmartDashboard.putBoolean("Lifter at end?", false);
+		} else if (getPosition() <= minLength) {
+			motor.set(0.2);
+			SmartDashboard.putBoolean("Lifter at end?", true);
+		} else if (maxLength <= getPosition()) {
+			motor.set(-0.2);
+			SmartDashboard.putBoolean("Lifter at end?", true);
+		}
+	}
+
+	@Override
+	public void setSetpoint(double angle) {
+		if (minLength < angle && angle < maxLength) {
+			super.setSetpoint(angle);
+			System.out.println("Setting Lifter Setpoint to " + angle);
+		} else if (angle <= minLength) {
+			super.setSetpoint(minLength);
+		} else if (maxLength <= angle) {
+			super.setSetpoint(maxLength);
 		}
 	}
 
 	public void setMotor(double power) {
-		if (!getPIDController().isEnabled()) {
+		if (getPIDController().isEnabled()) {
+			System.out.println("Lifter PID enabled; not setting motor.");
+		} else {
 			if (minLength < getPosition() && getPosition() < maxLength) {
 				motor.set(power);
 				SmartDashboard.putBoolean("Lifter at end?", false);
-			} else {
-				motor.set(0);
+			} else if (getPosition() <= minLength) {
+				motor.set(0.2);
+				SmartDashboard.putBoolean("Lifter at end?", true);
+			} else if (maxLength <= getPosition()) {
+				motor.set(-0.2);
 				SmartDashboard.putBoolean("Lifter at end?", true);
 			}
-		} else {
-			System.out.println("Lifter PID enabled; not setting motor.");
 		}
 	}
 }
